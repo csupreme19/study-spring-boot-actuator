c8897706543b7d896f2eaab2f9fa6abda2ee950d
csupreme19 <csupreme19@gmail.com>
2023-03-24 14:34:36 +0900

    커스텀 메트릭을 위한 주문 서비스

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,10 +1,13 @@
 package hello;
 
+import hello.order.v0.OrderConfigV0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
+@Import(OrderConfigV0.class)
 @SpringBootApplication
 public class ActuatorApplication {
 
